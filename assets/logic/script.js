30897a9e5bd3ea3e7dab9ca76c4b75badb245d64
Patrick Dunn <pmdunn78@gmail.com>
2021-01-31 12:10:00 -0600

    running screen check upon initialization

--- assets/logic/script.js
+++ assets/logic/script.js
@@ -1,5 +1,24 @@
 $(document).ready(() => {
+    var screen = $(window);
+    
+    const check = () => {
+        if (screen.width() < 600) {
+            $('.current-day').removeClass('bg-primary').addClass('bg-dark');
+        };
+        
+        if (screen.width() > 600) {
+            $('.current-day').removeClass('bg-dark').addClass('bg-primary');
+        }
+    };
+
+    check();
     console.log('script ready!')
+     
+    screen.resize(() => {
+        check();
+    });
+
+    
 
     const citiesList = ['New York', 'Seattle', 'Washington, D.C.', 'Queretaro'];
 
